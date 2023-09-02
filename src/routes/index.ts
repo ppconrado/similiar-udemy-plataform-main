36046c5daf4360f7c034dbea6610b5be4005ea34
ppconrado <ppconrado@yahoo.com.br>
2023-09-01 22:33:47 -0300

    chore: deploy set-up 2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,24 @@
-import { Router } from "express";
-import { studentRoutes } from "./student.routes";
-import { instructorRoutes } from "./instructor.routes";
-import { adminRoutes } from "./admin.routes";
-import swaggerUI from "swagger-ui-express";
-import YAML from "yamljs";
-const swaggerDocument = YAML.load("./swagger.yaml");
+import { Router } from 'express';
+import { studentRoutes } from './student.routes';
+import { instructorRoutes } from './instructor.routes';
+import { adminRoutes } from './admin.routes';
+import swaggerUI from 'swagger-ui-express';
+import YAML from 'yamljs';
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const routes = Router();
 
-routes.use("/api/v1/student", studentRoutes);
-routes.use("/api/v1/instructor", instructorRoutes);
-routes.use("/api/v1/admin", adminRoutes);
-routes.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+routes.use('/api/v1/student', studentRoutes);
+routes.use('/api/v1/instructor', instructorRoutes);
+routes.use('/api/v1/admin', adminRoutes);
+
+routes.get('/', (req, res) => {
+  res.send(
+    '<h1>Udemy School API</h1><a href="/api/v1/docs">Swagger Documentation</a>'
+  );
+});
+// routes.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+routes.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 export { routes };
