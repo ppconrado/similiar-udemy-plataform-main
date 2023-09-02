68935e9672c643108b817fab1e4308d21bbebb07
ppconrado <ppconrado@yahoo.com.br>
2023-09-01 22:15:33 -0300

    chore: deploy set-up

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import { config } from "dotenv";
-import { app } from "./server";
-import MongoDBService from "./services/MongoDBService";
-import { AdminRepository } from "./repositories/Admin";
+import { config } from 'dotenv';
+import { app } from './server';
+import MongoDBService from './services/MongoDBService';
+import { AdminRepository } from './repositories/Admin';
 
 config();
 
 const mongoURI = process.env.MONGO_URI;
 
 if (!mongoURI) {
-  throw new Error("MONGO_URI not found in environment variables");
+  throw new Error('MONGO_URI not found in environment variables');
 }
 
 const mongoDBService = new MongoDBService(mongoURI);
@@ -16,11 +16,13 @@ const mongoDBService = new MongoDBService(mongoURI);
 (async () => {
   try {
     await mongoDBService.connect();
-    console.log("Connected to MongoDB 🍃");
+    console.log('Connected to MongoDB 🍃');
     const adminRepository = new AdminRepository();
     adminRepository.createDefaultAdmin();
-    app.listen(3000, () => console.log(`Server is running in port 3000 🚀`));
+    app.listen(process.env.SERVER_PORT || 3000, () =>
+      console.log(`Server is running in port 3000 🚀`)
+    );
   } catch (error) {
-    console.error("Error starting server:", error);
+    console.error('Error starting server:', error);
   }
 })();
